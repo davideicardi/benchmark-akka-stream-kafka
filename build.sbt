865837aa6b7b4688e4ea2df96fe25ad1f7c6e9f1
Davide Icardi <davide.icardi@gmail.com>
2019-06-08 12:13:16 +0200

    WIP

--- build.sbt
+++ build.sbt
@@ -8,7 +8,14 @@ scalaVersion := "2.12.8"
 val akkaVersion = "2.5.22"
 val akkaStreamKafkaVersion = "1.0.3"
 val typesafeConfigVersion = "1.3.3"
+val log4jVersion = "2.11.1"
+val slf4jVersion = "1.7.25"
 
 libraryDependencies += "com.typesafe" % "config" % typesafeConfigVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % akkaVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-stream-kafka" % akkaStreamKafkaVersion
+libraryDependencies += "org.slf4j" % "slf4j-api" % slf4jVersion
+libraryDependencies += "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
+libraryDependencies += "org.apache.logging.log4j" % "log4j-slf4j-impl" % log4jVersion
+libraryDependencies += "org.apache.logging.log4j" % "log4j-api" % log4jVersion
+libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % log4jVersion
--- src/main/scala/SampleProducer.scala
+++ src/main/scala/SampleProducer.scala
@@ -1,13 +1,58 @@
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+import java.util.UUID
+
 import akka.actor.ActorSystem
+import akka.kafka.ProducerMessage.MultiMessage
 import akka.kafka.ProducerSettings
-import org.apache.kafka.common.serialization.ByteArraySerializer
+import akka.kafka.scaladsl.Producer
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.{RecordHeader, RecordHeaders}
+import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
+
+import scala.collection.immutable.Seq
+import scala.concurrent.ExecutionContextExecutor
+import scala.util.Random
 
 object SampleProducer {
   def main(args: Array[String]): Unit = {
-    val system = ActorSystem("SampleProducer")
 
-    val config = system.settings.config.getConfig("akka.kafka.producer")
-    val producerSettings = ProducerSettings(config, new ByteArraySerializer, new ByteArraySerializer)
-        .withBootstrapServers(bootstrapServers)
+    implicit val system: ActorSystem = ActorSystem("SampleProducer")
+    implicit val contextExecutor: ExecutionContextExecutor = system.dispatcher
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+    val config = system.settings.config
+    val topicName = config.getString("sampleTopic")
+    val numberOfKeys = config.getInt("numberOfKeys")
+    val keys = Seq.fill(numberOfKeys)(UUID.randomUUID().toString)
+    val recordBody = Files.readAllBytes(Paths.get(config.getString("recordBodyFile")))
+
+    val producerSettings = ProducerSettings(
+      config.getConfig("akka.kafka.producer"),
+      new StringSerializer,
+      new ByteArraySerializer)
+
+    Source.repeat(recordBody)
+      .map { value =>
+
+        val key = keys(Random.nextInt(keys.length))
+
+        val sampleHeader1 = new RecordHeader("sampleHeader1", key.getBytes(StandardCharsets.UTF_8))
+        val recordHeaders = new RecordHeaders()
+        recordHeaders.add(sampleHeader1)
+
+        MultiMessage[String, Array[Byte], Array[Byte]](
+          Seq(
+            new ProducerRecord(topicName, None.orNull, key, recordBody, recordHeaders)
+          ),
+          value
+        )
+      }
+      .via(Producer.flexiFlow(producerSettings))
+      .log("SampleProducer")
+      .runWith(Sink.ignore)
+
   }
 }
